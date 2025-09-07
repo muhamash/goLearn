5ed30a199ca9c8377d8a2e5e8a5c22218d3537f0
muhamsh <muhammad-ashraful@outlook.com>
2025-09-08 00:23:51 +0600

    basic data type

---
+++ cmd/learn/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello world")
+
+	var intNum uint64 = 4234234234232342334
+	intNum = intNum + 3456345234534345323
+	fmt.Println(intNum)
+
+	var floatNum float32 = 23423.2323452345
+	fmt.Println(floatNum)
+}
