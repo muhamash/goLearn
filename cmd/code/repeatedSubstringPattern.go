1cdc6f7374578c6d3569c1277c349366afeb8c76
muhamsh <muhammad-ashraful@outlook.com>
2025-09-14 23:45:01 +0600

    go file oka

--- cmd/code/repeatedSubstringPattern.go
+++ cmd/code/repeatedSubstringPattern.go
@@ -1,9 +1,17 @@
 package code
 
-import "strings"
+import (
+	"strings"
+)
 
 func repeatedSubstringPattern(s string) bool {
 	doubleString := s + s
 
 	return  strings.Contains(doubleString[1:len(doubleString)-1],s)
 }
+
+// func main() {
+// 	fmt.Println(repeatedSubstringPattern("abab"))       // true
+// 	fmt.Println(repeatedSubstringPattern("aba"))        // false
+// 	fmt.Println(repeatedSubstringPattern("abcabcabcabc")) // true
+// }
