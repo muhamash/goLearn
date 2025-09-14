1d3f687dda3920e8cad84048529199b2f790db45
muhamsh <muhammad-ashraful@outlook.com>
2025-09-14 22:19:46 +0600

    strStr manual

--- cmd/code/strStr.go
+++ cmd/code/strStr.go
@@ -1,7 +1,5 @@
 package code
 
-import "strings"
-
 func strStr(haystack string, needle string) int {
 	if len(haystack) == 0 {
 		return 0
@@ -11,5 +9,20 @@ func strStr(haystack string, needle string) int {
 		return  0
 	}
 
-	return  strings.Index(haystack, needle)
+	// return  strings.Index(haystack, needle)
+
+	for i := 0; i <= len(haystack) - len(needle); i++ {
+		j := 0
+
+		for  j < len(haystack) && haystack[i+j] == needle[j] {
+			j++
+		}
+
+		if j == len(needle) {
+			return  i
+		}
+	}
+
+
+    return -1
 }
--- cmd/code/strStr.js
+++ cmd/code/strStr.js
@@ -5,7 +5,26 @@ function strStr(haystack, needle) {
         return false
     }
 
-    return haystack.indexOf(needle)
+    // return haystack.indexOf( needle )
+
+    for ( let i = 0; i <= haystack.length - needle.length - 1; i++ )
+    {
+        let j = 0;
+        while ( j < haystack.length && haystack[ i + j ] === needle[ j ] )
+        {
+            j++
+        }
+
+        if ( j === needle.length )
+        {
+            return i
+        }
+
+
+        return -1
+    }
+    
+
 }
 
 console.log(strStr("sadbutsad", "sad"));
